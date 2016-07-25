22012263bd87a02690e7eb407bd14fd8605d65b9
Liang Wang <ryanrhymes@gmail.com>
2016-07-25 15:58:46 +0100

    coding ...

---
+++ lib/client.ml
@@ -0,0 +1,18 @@
+(** [ client ]
+  registers to the server
+*)
+
+let context = ZMQ.Context.create () in
+print_endline "Connecting to hello world server...";
+let requester = ZMQ.Socket.create context ZMQ.Socket.req in
+ZMQ.Socket.connect requester "tcp://localhost:5555";
+
+for i = 1 to 10 do
+  Printf.printf "Sending request %d...\n" i;
+  ZMQ.Socket.send requester "Hello";
+  let reply = ZMQ.Socket.recv requester in
+  Printf.printf "Received reply %d: [%s]\n" i reply
+done;
+
+ZMQ.Socket.close requester;
+ZMQ.Context.terminate context
---
+++ lib/server.ml
@@ -0,0 +1,16 @@
+(** [ Server ]
+  Handle actor registration
+*)
+
+let context = ZMQ.Context.create () in
+let responder = ZMQ.Socket.create context ZMQ.Socket.rep in
+ZMQ.Socket.bind responder "tcp://*:5555";
+
+while true do
+  let request = ZMQ.Socket.recv responder in
+  Printf.printf "Received request: [%s]\n%!" request;
+  ZMQ.Socket.send responder "World"
+done;
+
+ZMQ.Socket.close responder;
+ZMQ.Context.terminate context
