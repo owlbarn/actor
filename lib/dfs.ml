377bd2fdcec7caa6e83a4556abe5e18e129e28ae
Liang Wang <ryanrhymes@gmail.com>
2016-08-04 22:49:51 +0300

    coding ...

--- lib/dfs.ml
+++ lib/dfs.ml
@@ -2,29 +2,16 @@
   is a distributed file system for actors
 *)
 
-(* TODO: this module needs some redesign ...
-  think about how to abstract distributed datasets, compare to rdd
-*)
-
 open Types
 
-type t = {
-  did : string;            (* data id *)
-  uid : string;            (* user id *)
-  size : int;              (* size in bytes *)
-  service : string list;   (* associated services *)
-}
+type t = Obj.t
 
 let _data = ref StrMap.empty
 
-let add did uid =
-  _data := StrMap.add did {
-    did = did; uid = uid; size = 0;
-    service = [];
-  } !_data
+let add id data = _data := StrMap.add id data !_data
 
-let mem did = StrMap.mem did !_data
+let mem id = StrMap.mem id !_data
 
-let remove did = StrMap.remove did !_data
+let remove id = StrMap.remove id !_data
 
-let size did = (StrMap.find did !_data).size
+let size id = Obj.size (StrMap.find id !_data)
