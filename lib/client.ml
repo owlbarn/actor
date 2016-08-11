43354b9e220724248beb0bc780ca6e7f618864c3
Liang Wang <ryanrhymes@gmail.com>
2016-08-11 23:50:58 +0100

    new utils module

--- lib/client.ml
+++ lib/client.ml
@@ -17,25 +17,28 @@ let register id u_addr m_addr =
   ZMQ.Socket.close req
 
 let heartbeat id m_addr =
+  print_endline "send heartbeat ...";
   let req = ZMQ.Socket.create _ztx ZMQ.Socket.req in
   ZMQ.Socket.connect req m_addr;
   ZMQ.Socket.send req (to_msg Heartbeat [|id|]);
   ignore (ZMQ.Socket.recv req);
+  print_endline "end heartbeat ...";
   ZMQ.Socket.close req
 
-let run id =
+let run id addr =
   register myid addr manager;
   (* set up local service *)
   let rep = ZMQ.Socket.create _ztx ZMQ.Socket.rep in
+  ZMQ.Socket.set_receive_timeout rep 5000;
   ZMQ.Socket.bind rep addr;
   while true do
-    (* TODO: add more logic *)
-    let m = of_msg (ZMQ.Socket.recv rep) in
-    match m.typ with
-    | _ -> ()
+    try let m = of_msg (ZMQ.Socket.recv rep) in
+      match m.typ with
+      | _ -> ()
+    with exn -> heartbeat myid addr
   done;
   ZMQ.Socket.close rep;
   ZMQ.Context.terminate _ztx
 
 
-let () = run myid
+let () = run myid addr
