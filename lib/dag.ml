ebe7e86ddcad4b5b6885ddb054fc93dd506d4add
Liang Wang <ryanrhymes@gmail.com>
2016-08-16 23:48:43 +0100

    coding

--- lib/dag.ml
+++ lib/dag.ml
@@ -2,10 +2,24 @@
   maintains a directed acyclic graph of computation.
 *)
 
+open Graph.Imperative
+
 type t = {
   data_id : string;
   finshed : bool;
   parents : (string * string) list;
 }
+
+module V = struct
+  type t = string
+  let compare = Pervasives.compare
+  let hash = Hashtbl.hash
+  let equal = (=)
+end
+module E = struct
+  type t = float
+  let compare = Pervasives.compare
+  let default = 0.0
+end
 
 let _graph : t array = [||]
