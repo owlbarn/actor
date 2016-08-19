9722353d9f35b10a139e14590f5f3e19d670e6b8
Liang Wang <ryanrhymes@gmail.com>
2016-08-19 14:56:12 +0100

    coding ...

--- lib/context.ml
+++ lib/context.ml
@@ -7,15 +7,16 @@ open Types
 type t = {
   mutable jid : string;
   mutable master : string;
-  mutable workers : [ `Req ] ZMQ.Socket.t list;
+  mutable w_addr : string list;
+  mutable w_sock : [ `Req ] ZMQ.Socket.t list;
 }
 
-let _context = { jid = ""; master = ""; workers = []; }
+let _context = { jid = ""; master = ""; w_addr = []; w_sock = []; }
 let my_addr = "tcp://127.0.0.1:" ^ (string_of_int (Random.int 5000 + 5000))
 let _ztx = ZMQ.Context.create ()
 
 let _broadcast_all t s =
-  List.iter (fun req -> ZMQ.Socket.send req (to_msg t s)) _context.workers
+  List.iter (fun req -> ZMQ.Socket.send req (to_msg t s)) _context.w_sock
 
 let barrier l =
   let set = List.map (fun x -> x, ZMQ.Poll.In) l |> Array.of_list |> ZMQ.Poll.mask_of in
@@ -68,11 +69,11 @@ let master_fun m =
   (* wait until all the allocated actors register *)
   let rep = ZMQ.Socket.create _ztx ZMQ.Socket.rep in
   ZMQ.Socket.bind rep my_addr;
-  while (List.length _context.workers) < (List.length addrs) do
+  while (List.length _context.w_sock) < (List.length addrs) do
     let m = ZMQ.Socket.recv rep in
     let s = ZMQ.Socket.create _ztx ZMQ.Socket.req in
     ZMQ.Socket.connect s m;
-    _context.workers <- (s :: _context.workers);
+    _context.w_sock <- (s :: _context.w_sock);
     ZMQ.Socket.send rep "";
   done; ZMQ.Socket.close rep
 
@@ -139,7 +140,7 @@ let run_job () =
   List.iter (fun s ->
     let s' = List.map (fun x -> Dag.get_vlabel_f x) s in
     _broadcast_all Pipeline (Array.of_list s');
-    barrier _context.workers;
+    barrier _context.w_sock;
     Dag.mark_stage_done s;
   ) (Dag.stages ())
 
@@ -171,23 +172,23 @@ let collect x =
   Utils.logger ("collect " ^ x ^ "\n");
   run_job ();
   _broadcast_all Collect [|x|];
-  barrier _context.workers
+  barrier _context.w_sock
 
 let count x =
   Utils.logger ("count " ^ x ^ "\n");
   run_job ();
   _broadcast_all Count [|x|];
-  barrier _context.workers |> List.fold_left (+) 0
+  barrier _context.w_sock |> List.fold_left (+) 0
 
 let terminate () =
   Utils.logger ("terminate job " ^ _context.jid ^ "\n");
   _broadcast_all Terminate [||];
-  barrier _context.workers
+  barrier _context.w_sock
 
 let broadcast x =
-  Utils.logger ("broadcast -> " ^ string_of_int (List.length _context.workers) ^ " workers\n");
+  Utils.logger ("broadcast -> " ^ string_of_int (List.length _context.w_sock) ^ " workers\n");
   let y = Memory.rand_id () in
   _broadcast_all Broadcast [|Marshal.to_string x []; y|];
-  barrier _context.workers; y
+  barrier _context.w_sock; y
 
 let get_value x = Memory.find x
