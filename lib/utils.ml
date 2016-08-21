0cc6200b6139c4d541ca8641149f00142cc0d97b
Liang Wang <ryanrhymes@gmail.com>
2016-08-21 12:49:59 +0100

    coding ...

--- lib/utils.ml
+++ lib/utils.ml
@@ -7,13 +7,11 @@ open Types
 let logger s =
   let open Unix in
   let t = gmtime (time ()) in
-  Printf.printf "[#%i %02i:%02i:%02i] %s\n%!"
-    (getpid ()) t.tm_hour t.tm_min t.tm_sec s
+  Printf.printf "[#%i %02i:%02i:%02i] %s\n%!" (getpid ()) t.tm_hour t.tm_min t.tm_sec s
 
 let group_by_key x =
-  let d = ref StrMap.empty in
-  List.iter (fun (k,v) ->
-    let k = Marshal.to_string k [] in
-    let u = if StrMap.mem k !d then StrMap.find k !d else [] in
-    d := StrMap.add k (u @ [(k,v)]) !d ) x;
-  StrMap.bindings !d
+  let h, g = Hashtbl.(create 1024, create 1024) in
+  List.iter (fun (k,v) -> Hashtbl.(add h k v; if not (mem g k) then add g k None)) x;
+  Hashtbl.fold (fun k v l -> (k,(List.map (fun v -> (k,v)) Hashtbl.find_all h k)) :: l) g []
+
+let flatten_kvg x = List.map (fun (k,l) -> List.map (fun v -> (k,v)) l) x |> List.flatten
