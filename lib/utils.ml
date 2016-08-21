d28858d6da886e32c1ce8a9ed2571bbf19d36139
Liang Wang <ryanrhymes@gmail.com>
2016-08-21 01:45:56 +0100

    coding ...

--- lib/utils.ml
+++ lib/utils.ml
@@ -2,8 +2,18 @@
   Some shared helper functions
 ]  *)
 
+open Types
+
 let logger s =
   let open Unix in
   let t = gmtime (time ()) in
   Printf.printf "[#%i %02i:%02i:%02i] %s\n%!"
     (getpid ()) t.tm_hour t.tm_min t.tm_sec s
+
+let group_by_key x =
+  let d = ref StrMap.empty in
+  List.iter (fun (k,v) ->
+    let k = Marshal.to_string k [] in
+    let u = if StrMap.mem k !d then StrMap.find k !d else [] in
+    d := StrMap.add k (u @ [(k,v)]) !d ) x;
+  StrMap.bindings !d
