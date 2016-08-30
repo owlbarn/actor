5506573181df4c3e59c1ae1895451a44ddbb3941
Liang Wang <ryanrhymes@gmail.com>
2016-08-30 23:59:55 +0100

    coding ...

--- test/test_kmeans.ml
+++ test/test_kmeans.ml
@@ -7,8 +7,7 @@ let print_points = List.iter (fun x -> Printf.printf "(%.2f,%.2f)\n" (fst x) (sn
 let distance x y = (((fst x) -. (fst y)) ** 2.) +. (((snd x) -. (snd y)) ** 2.)
 let add_2pts x y = ( ((fst x)+.(fst y)), ((snd x)+.(snd y)) )
 
-let format_filter_data fname =
-  fname
+let format_filter_data fname = fname
   |> Ctx.map Str.(split (regexp "[\r\n]"))
   |> Ctx.flatten
   |> Ctx.map Str.(split (regexp "[,]"))
@@ -20,12 +19,10 @@ let kmeans x =
   for i = 0 to 100 do
     let bc = Ctx.broadcast !centers in
     let y = Ctx.map (fun x ->
-      let centers = Ctx.get_value bc in
       let k = ref (-1, max_float) in
-      List.iteri (fun i y ->
+      Ctx.get_value bc |> List.iteri (fun i y ->
         let d = distance x y in
-        if d < (snd !k) then k := (i,d)
-      ) centers;
+        if d < (snd !k) then k := (i,d) );
       (fst !k, (x, 1.)) ) x in
     let y = Ctx.reduce_by_key (fun x y -> (add_2pts (fst x) (fst y), (snd x)+.(snd y)) ) y in
     centers := Ctx.collect y
