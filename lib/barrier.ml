075e11f4c597b31604e251c1b636bce614574bd9
Liang Wang <ryanrhymes@gmail.com>
2016-09-09 11:15:06 +0100

    coding in barrier module

--- lib/barrier.ml
+++ lib/barrier.ml
@@ -1,3 +1,16 @@
 (** [ Barrier module ]
   provides flexible synchronisation barrier controls.
 *)
+
+let _bcache = Hashtbl.create 1_000_000
+let _fcache = Hashtbl.create 1_000_000
+
+let add bar res = Hashtbl.add _bcache bar res
+
+let create_bar f =
+let bar = Random.int 536870912 in
+Hashtbl.add _fcache bar f; bar
+
+let _ =
+  Hashtbl.add _bcache 1 "hello";
+  Hashtbl.add _fcache 1 "hello"
