295accab4481980002107de6ced3382138fe0258
Liang Wang <ryanrhymes@gmail.com>
2016-09-15 16:30:39 +0100

    coding ...

--- lib/parameter.ml
+++ lib/parameter.ml
@@ -2,8 +2,29 @@
   provides a global variable like kv store
 *)
 
-let get x = None
+Logger.update_config Config.level Config.logdir ""
 
-let set x = None
+let _param = Hashtbl.create 1_000_000
+
+let get k t =
+  let v, t' = Hashtbl.find _param (Obj.repr k) in
+  Logger.debug "%i" (t' - t);
+  v
+
+let set k v t =
+  let v, t' = Hashtbl.find _param (Obj.repr k) in
+  Logger.debug "%i" (t' - t);
+  Hashtbl.replace _param (Obj.repr k) (v, t)
 
 let schedule x = None
+
+
+(** FIXME: for debug purpose *)
+
+let _ =
+  Hashtbl.add _param (Obj.repr 5) ("abc", 123);
+  Hashtbl.add _param (Obj.repr 6) ("def", 125);
+  let _ = get 5 124 in
+  let _ = get 6 123 in
+  let _ = set 6 "ccc" 126 in
+  ()
