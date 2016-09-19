ce47a381d48b0d50c305e5776b627e35aa8ba220
Liang Wang <ryanrhymes@gmail.com>
2016-09-19 22:26:47 +0100

    clean up memory module

--- lib/memory.ml
+++ lib/memory.ml
@@ -4,9 +4,7 @@
 
 open Types
 
-type t = Obj.t
-
-let _data = Hashtbl.create 10_000_000
+let _data : (string, Obj.t) Hashtbl. t = Hashtbl.create 10_000_000
 
 let rand_id () = string_of_int (Random.int 536_870_912)
 
@@ -19,18 +17,3 @@ let remove id = Hashtbl.remove _data id
 let find id = Obj.obj (Hashtbl.find _data id)
 
 let size id = Obj.size (find id)
-
-let load id fname =
-  let l = Unix.((stat fname).st_size) in
-  let b = Bytes.create l in
-  let f = Unix.openfile fname [] 0o644 in
-  let _ = Unix.read f b 0 l in
-  add id [ b ]
-
-(* FIXME: debug purpose *)
-
-let t () =
-  let d = Array.init 5000 (fun x -> Random.float 10.) in
-  add "default" (Array.to_list d);
-  load "wordcount.data" "./data/wordcount.data";
-  load "kmeans.data" "./data/kmeans.data"
