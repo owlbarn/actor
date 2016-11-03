b44015d5e8bead7dd49c7b6e32912d52d7d36da0
Liang Wang <ryanrhymes@gmail.com>
2016-11-03 14:57:04 +0000

    coding ...

--- test/test_sgd.ml
+++ test/test_sgd.ml
@@ -1,11 +1,19 @@
 (** [ Test coordinate descent ]  *)
 
 open Owl
+module MX = Dense.Real
 module PS = Parameter
 
 let param = Array.(init 1000 (fun x -> x) |> to_list)
 
 let pivot = ref 0
+
+let calc_gradient x y p b g =
+  let x, i = MX.draw_rows x b in
+  let y = MX.rows y i in
+  let y' = MX.(x $@ p) in
+  let d = g x y y' in
+  d
 
 let schedule workers =
   Logger.debug "scheduling ...";
@@ -28,16 +36,19 @@ let pull vars =
 let test_sgd () =
   PS.register_schedule schedule;
   PS.register_pull pull;
-  PS.register_push push;  
+  PS.register_push push;
   PS.start Sys.argv.(1) Config.manager_addr;
   Logger.info "do some work at master node"
 
 (* prepare some synthetic data *)
 let load_data () =
-  let x = Dense.Real.uniform 1000 3 in
-  let p = Dense.Real.uniform 3 1 in
-  let b = Dense.Real.gaussian ~sigma:0.05 1000 1 in
-  let y = Dense.Real.((x $@ p) +@ b) in
+  let x = MX.uniform 1000 3 in
+  let p = MX.uniform 3 1 in
+  let b = MX.gaussian ~sigma:0.05 1000 1 in
+  let y = MX.((x $@ p) +@ b) in
   x, y
+
+let load_model () = None
+
 
 let _ = test_sgd ()
