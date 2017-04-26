db6f930d63a9365cba7fbfb30db97cba1ea5aa9b
Liang Wang <ryanrhymes@gmail.com>
2017-04-26 14:47:58 +0100

    fix inconsistency with new owl interface

--- test/test_peer_lda.ml
+++ test/test_peer_lda.ml
@@ -6,10 +6,10 @@ open Types
 (* load stopwords, load data, build dict, tokenisation *)
 let s = Dataset.load_stopwords ()
 let x = Dataset.load_nips_train_data s
-let v = Owl_topic_utils.build_vocabulary x
+let v, _ = Owl_nlp_utils.build_vocabulary x
 (* only choose 30% data to train
 let x = Stats.choose x (Array.length x / 3) *)
-let d = Owl_topic_utils.tokenisation v x
+let d = Owl_nlp_utils.tokenise_all v x
 let t = 100
 
 let _ =
